9e09fd64def9175345270f97a13a0db47c18ce93
lnxg33k <ahmedelantry@gmail.com>
2018-05-23 14:08:06 +0300

    Get the apache php.ini instead of the CLI

--- drupalHarden.sh
+++ drupalHarden.sh
@@ -8,8 +8,7 @@ disable_sys_funcs() {
 	printf "\nDisabling PHP Dangerous/Insecure Functions ...\n"
 
 	funcs_to_disable="apache_child_terminate, apache_setenv, define_syslog_variables, escapeshellarg, escapeshellcmd, eval, exec, fp, fput, ftp_connect, ftp_exec, ftp_get, ftp_login, ftp_nb_fput, ftp_put, ftp_raw, ftp_rawlist, highlight_file, ini_alter, ini_get_all, ini_restore, inject_code, mysql_pconnect, openlog, passthru, php_uname, phpAds_remoteInfo, phpAds_XmlRpc, phpAds_xmlrpcDecode, phpAds_xmlrpcEncode, popen, posix_getpwuid, posix_kill, posix_mkfifo, posix_setpgid, posix_setsid, posix_setuid, posix_setuid, posix_uname, proc_close, proc_get_status, proc_nice, proc_open, proc_terminate, shell_exec, syslog, system, xmlrpc_entity_decode, phpinfo"
-	PHP_ini=$(php -i | grep -oE "\s+/.*?php.ini" | sed -e 's/^[ \t]*//')
-
+	PHP_ini=$(find /etc/ -name php.ini 2>&1 | grep apache)
 	if [[ -f $PHP_ini ]]; then
 		printf "php.ini path was found => $PHP_ini\n"
 		echo "; PHP security hardening by $(whoami) at $(date)" >>$PHP_ini
